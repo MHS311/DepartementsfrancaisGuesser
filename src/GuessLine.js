eda95a9006c74735c7a6b797844ec1a8d87cdf48
Matthis Ho-Sun <matthis.ho-sun@grenoble-inp.org>
2026-01-07 17:24:14 +0100

    init

---
+++ src/GuessLine.js
@@ -0,0 +1,14 @@
+import './GuessLine.js';
+import { React } from "react";
+
+function GuessLine({key, guess, given}) {
+  return (
+    <tr>
+      <td className="code">{(given == 0 || given == -1) ? <strong>{guess.properties.code}</strong> : <input type="text" name="numero" />}</td>
+      <td className="name">{(given == 1 || given == -1) ? <strong>{guess.properties.nom}</strong> : <input type="text" name="departement" />}</td>
+      <td className="chef-lieu">{(given == 2 || given == -1) ? <strong>{guess.properties["chef-lieu"]}</strong> : <input type="text" name="chef_lieu" />}</td>
+    </tr>
+  )
+}
+
+export default GuessLine;
